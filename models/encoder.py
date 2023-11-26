1e4700ca351540624268e7ec59e909d90b64f4b0
Joel Lapin <joellapin@comcast.net>
2023-11-26 13:31:48 +0100

    encoder.py translated over and confirmed to work in 2 different setting: subdivide=True, use_mass=False, use_charge=False subdivide=True, use_mass=True, use_charge=True

--- models/encoder.py
+++ models/encoder.py
@@ -1,18 +1,13 @@
 
-from copy import deepcopy
-import numpy as np
-from utils import Scale
-import models.model_parts as mp
-import tensorflow as tf
-K = tf.keras
-L = K.layers
-A = K.activations
-I = K.initializers
-gelu = tf.keras.activations.gelu
+import model_parts as mp
+import torch as th
+from torch import nn
 
-class Encoder(K.Model):
+class Encoder(nn.Module):
     def __init__(self,
+                 in_units=2, # input units from mz/ab tensor
                  running_units=512, # num units running throughout model
+                 sequence_length=100, # maximum number of peaks
                  mz_units=512, # units in mz fourier vector
                  ab_units=256, # units in ab fourier vector
                  subdivide=False, # subdivide mz units in 2s and expand-concat
@@ -31,6 +26,7 @@ class Encoder(K.Model):
                  ):
         super(Encoder, self).__init__()
         self.run_units = running_units
+        self.sl = sequence_length
         self.mz_units = mz_units
         self.ab_units = ab_units
         self.subdivide = subdivide
@@ -47,44 +43,51 @@ class Encoder(K.Model):
         self.its = recycling_its
         
         # Position modulation
-        self.alpha = tf.Variable(0.1, trainable=True)
+        self.alpha = nn.Parameter(th.tensor(0.1), requires_grad=True)
         
-        self.MzSeq = K.Sequential([
-            L.Dense(mz_units // 4),
-            mp.KerasActLayer(tf.nn.swish),
-        ])
-
+        mdim = mz_units//4 if subdivide else mz_units
+        self.MzSeq = nn.Sequential(
+            nn.Linear(mdim, mdim), nn.SiLU()
+        )
+        
         # charge/energy/mass embedding transformation
         self.atleast1 = use_charge or use_energy or use_mass
         if self.atleast1:
-            self.ce_emb = K.Sequential([
-                L.Dense(ce_units), mp.KerasActLayer(tf.nn.swish)
-            ])
-
+            num = sum([use_charge, use_energy, use_mass])
+            self.ce_emb = nn.Sequential(
+                nn.Linear(ce_units*num, ce_units), nn.SiLU()
+            )
+        
         # First transformation
-        self.first = L.Dense(running_units)#, kernel_initializer=I.Zeros())
+        self.first = nn.Linear(mz_units+ab_units, running_units)
         
         # Main block
-        attention_dict = {'d': att_d, 'h': att_h}
-        ffn_dict = {'unit_multiplier': ffn_multiplier}
+        attention_dict = {'indim': running_units, 'd': att_d, 'h': att_h}
+        ffn_dict = {'indim': running_units, 'unit_multiplier': ffn_multiplier}
         is_embed = True if self.atleast1 else False
-        self.main = [
+        self.main = nn.ModuleList([
             mp.TransBlock(
-                attention_dict, ffn_dict, norm_type, prenorm, is_embed, preembed
+                attention_dict, 
+                ffn_dict, 
+                norm_type, 
+                prenorm, 
+                is_embed, 
+                ce_units,
+                preembed
             ) 
             for _ in range(depth)
-        ]
-        self.main_proj = A.linear#L.Dense(embedding_units, kernel_initializer=I.Zeros())
+        ])
+        self.main_proj = nn.Identity()#L.Dense(embedding_units, kernel_initializer=I.Zeros())
         
         # Normalization type
         self.norm = mp.get_norm_type(norm_type)
         
         # Recycling embedder
-        self.recyc = K.Sequential([
-            self.norm() if prenorm else mp.KerasActLayer(A.linear),
-            L.Dense(running_units) if False else mp.KerasActLayer(A.linear),
-            mp.KerasActLayer(A.linear) if prenorm else self.norm()
-        ]) if self.its > 1 else A.linear
+        self.recyc = nn.Sequential(
+            self.norm(running_units) if prenorm else nn.Identity(),
+            nn.Linear(running_units, running_units) if False else nn.Identity(),
+            nn.Identity() if prenorm else self.norm(running_units)
+        ) if self.its > 1 else nn.Identity()
         
         """# Recycling modulator
         self.alphacyc = ( 
@@ -93,30 +96,19 @@ class Encoder(K.Model):
             tf.Variable(1.0, trainable=False)
         )"""
         
-        self.global_step = tf.Variable(0, trainable=False)
+        self.global_step = nn.Parameter(th.tensor(0), requires_grad=False)
         
-    def build(self, x):
-        self.sl = x[1]
-        
-        # Positional embedding
         self.pos = mp.FourierFeatures(
-            tf.range(self.sl, dtype=tf.float32), self.run_units, 5.*self.sl
+            th.arange(self.sl, dtype=th.float32), self.run_units, 5.*self.sl
         )
-        
-        """# Tag
-        self.tag = tf.zeros((1, self.sl), tf.int32)"""
-
-        """self.tensor = tf.Variable(
-            tf.random.normal((x[1], self.embed_units)), trainable=True 
-        )"""
     
-    """def PredictTag(self, predict_array):
-        return tf.cast(tf.one_hot(predict_array, 2), tf.float32)"""
-
+    def total_params(self):
+        return sum([m.numel() for m in self.parameters()])
+    
     def MzAb(self, x, inp_mask=None):
-        mz, ab = tf.split(x, 2, -1)
+        mz, ab = th.split(x, 1, -1)
         
-        mz = tf.squeeze(mz)
+        mz = mz.squeeze()
         if self.subdivide:
             mz = mp.subdivide_float(mz)
             minidim = self.mz_units//4
@@ -124,28 +116,25 @@ class Encoder(K.Model):
         else:
             mz_emb = mp.FourierFeatures(mz, self.mz_units, 10000.)
         mz_emb = self.MzSeq(mz_emb) # multiply sequential to mz fourier feature
-        mz_emb = tf.reshape(mz_emb, (x.shape[0], x.shape[1], -1))
+        mz_emb = mz_emb.reshape(x.shape[0], x.shape[1], -1)
         # ASSUME ab comes in 0-1, multiply by 100 (0-100) before expansion
         ab_emb = mp.FourierFeatures(100*ab[...,0], self.ab_units, 500.)
         
         # Apply input mask, if necessary
         if inp_mask is not None:
-            mz_mask, ab_mask = tf.split(inp_mask, 2, -1)
+            mz_mask, ab_mask = th.split(inp_mask, 1, -1)
             # Zeros out the feature's entire Fourier vector
             mz_emb *= mz_mask
             ab_emb *= ab_mask
             
-        out = tf.concat([mz_emb, ab_emb], axis=-1)
+        out = th.cat([mz_emb, ab_emb], dim=-1)
 
         return out
-    
-    def total_params(self):
-        return sum([np.prod(m.shape) for m in self.trainable_variables])
     
     def Main(self, inp, embed=None, mask=None):
         out = inp
         for layer in self.main:
-            out = layer(out, temb=embed, spec_mask=mask)
+            out = layer(out, embed_feats=embed, spec_mask=mask)
         return self.main_proj(out)
     
     def UpdateEmbed(self, 
@@ -161,12 +150,12 @@ class Encoder(K.Model):
                     ):
         # Create mask
         if length!=None:
-            grid = tf.tile(
-                tf.range(self.sl, dtype=tf.int32)[None], 
+            grid = th.tile(
+                th.arange(self.sl, dtype=th.int32)[None], 
                 (x.shape[0], 1)
             ) # bs, seq_len
             mask = grid >= length[:, None]
-            mask = 1e5*tf.cast(mask, tf.float32)
+            mask = (1e5*mask).type(th.float32)
         else:
             mask = None
         
@@ -180,14 +169,14 @@ class Encoder(K.Model):
         if self.atleast1:
             ce_emb = []
             if self.use_charge:
-                charge = tf.cast(charge, tf.float32)
+                charge = charge.type(th.float32)
                 ce_emb.append(mp.FourierFeatures(charge, self.ce_units, 10.))
             if self.use_energy:
                 ce_emb.append(mp.FourierFeatures(energy, self.ce_units, 150.))
             if self.use_mass:
                 ce_emb.append(mp.FourierFeatures(mass, self.ce_units, 20000.))
             # tf.concat works if list is 1 or multiple members
-            ce_emb = tf.concat(ce_emb, axis=-1)
+            ce_emb = th.cat(ce_emb, dim=-1)
             ce_emb = self.ce_emb(ce_emb)
         else:
             ce_emb = None
@@ -205,8 +194,8 @@ class Encoder(K.Model):
         output = (emb, mask) if return_mask else emb
         
         return output
-       
-    def call(self, 
+    
+    def forward(self, 
              x,
              charge=None, 
              energy=None, 
@@ -224,8 +213,8 @@ class Encoder(K.Model):
         # Recycled embedding
         emb = (
             emb 
-            if tf.is_tensor(emb) else 
-            tf.zeros((x.shape[0], self.sl, self.run_units))
+            if emb is not None else 
+            th.zeros(x.shape[0], self.sl, self.run_units)
         )
         
         for _ in range(its):
@@ -238,9 +227,4 @@ class Encoder(K.Model):
         Output['emb'] = emb
         if return_mask: Output['mask'] = mask
         
-        out = emb
-        
-        # output = out, mask if return_mask else out
-        
         return Output
-
