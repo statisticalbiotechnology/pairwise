77b76b0d05592e5f05fd406db0b57d16f52dd666
Alfred <alfredn@kth.se>
2024-03-09 11:50:37 +0100

    cfg

--- src/models/dc_encoder.py
+++ src/models/dc_encoder.py
@@ -219,6 +219,30 @@ def dc_encoder_base(
     )
     return model
 
+def dc_encoder_larger(
+    use_charge=False,
+    use_energy=False,
+    use_mass=False,
+    static_peak_encoder=False,
+    **kwargs,
+):
+    d_model = 1024
+    if static_peak_encoder:
+        peak_encoder = StaticPeakEncoder(d_model)
+    else:
+        peak_encoder = True
+    model = SpectrumTransformerEncoder(
+        d_model=d_model,
+        nhead=8,
+        dim_feedforward=2048,
+        n_layers=9,
+        use_charge=use_charge,
+        use_mass=use_mass,
+        use_energy=use_energy,
+        peak_encoder=peak_encoder,
+    )
+    return model
+
 def casanovo_encoder(
     use_charge=False,
     use_energy=False,
