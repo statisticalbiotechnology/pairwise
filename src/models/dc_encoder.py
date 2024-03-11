0f830db63532fcdf54d65f3531bf76b1652faad8
Alfred <alfredn@kth.se>
2024-03-12 00:56:21 +0100

    huge dc encoder

--- src/models/dc_encoder.py
+++ src/models/dc_encoder.py
@@ -244,6 +244,30 @@ def dc_encoder_larger(
     )
     return model
 
+def dc_encoder_huge(
+    use_charge=False,
+    use_energy=False,
+    use_mass=False,
+    static_peak_encoder=False,
+    **kwargs,
+):
+    d_model = 2048
+    if static_peak_encoder:
+        peak_encoder = StaticPeakEncoder(d_model)
+    else:
+        peak_encoder = True
+    model = SpectrumTransformerEncoder(
+        d_model=d_model,
+        nhead=8,
+        dim_feedforward=2048,
+        n_layers=18,
+        use_charge=use_charge,
+        use_mass=use_mass,
+        use_energy=use_energy,
+        peak_encoder=peak_encoder,
+    )
+    return model
+
 def casanovo_encoder(
     use_charge=False,
     use_energy=False,
