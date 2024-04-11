e7ad7426e18e0072e60d28272c952f5e2232eb3e
Alfred-N <alfredn@kth.se>
2024-04-11 13:05:00 +0200

    update lance dataloader to use the new distributed sampler with random shuffeling (buffer)

--- src/lance_data_module.py
+++ src/lance_data_module.py
@@ -1,13 +1,18 @@
+from functools import partial
 from typing import Callable
 import pytorch_lightning as pl
 from pathlib import Path
 
 import os.path as path
-from lance_dataset import LanceDataset
+from lance.torch.data import LanceDataset
+from lance.sampler import ShardedBatchSampler
+from lance_helper_fns import _to_batch_dict
 
 
 class LanceDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: Path, batch_size: int, collate_fn: Callable):
+    def __init__(
+        self, data_dir: Path, batch_size: int, collate_fn: Callable, seed: int = 0
+    ):
         super().__init__()
 
         self.data_dirs = [
@@ -27,43 +32,68 @@ class LanceDataModule(pl.LightningDataModule):
         )
         self.lance_dirs = lance_dirs
         self.batch_size = batch_size
-        self.collate_fn = collate_fn
+        # self.collate_fn = collate_fn
+        self.to_tensor_fn = partial(_to_batch_dict, collate_fn=collate_fn)
+        self.seed = seed
 
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
+
+            train_sampler = ShardedBatchSampler(
+                rank=self.trainer.global_rank,
+                world_size=self.trainer.world_size,
+                randomize=True,
+                seed=self.seed,
+            )
             self.train_dataset = LanceDataset(
                 self.lance_dirs[0],
                 batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
+                to_tensor_fn=self.to_tensor_fn,
+                sampler=train_sampler,
+                with_row_id=None,
+            )
+            val_sampler = ShardedBatchSampler(
                 rank=self.trainer.global_rank,
                 world_size=self.trainer.world_size,
+                randomize=False,
+                seed=self.seed,
             )
-            # Similarly set up validation and test datasets if needed
             self.val_dataset = LanceDataset(
                 self.lance_dirs[1],
                 batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                rank=self.trainer.global_rank,
-                world_size=self.trainer.world_size,
+                to_tensor_fn=self.to_tensor_fn,
+                sampler=val_sampler,
+                with_row_id=None,
             )
 
         if stage == "validate" or stage is None:
-            # Similarly set up validation and test datasets if needed
+            val_sampler = ShardedBatchSampler(
+                rank=self.trainer.global_rank,
+                world_size=self.trainer.world_size,
+                randomize=False,
+                seed=self.seed,
+            )
             self.val_dataset = LanceDataset(
                 self.lance_dirs[1],
                 batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
-                rank=self.trainer.global_rank,
-                world_size=self.trainer.world_size,
+                to_tensor_fn=self.to_tensor_fn,
+                sampler=val_sampler,
+                with_row_id=None,
             )
 
         if stage == "test" or stage is None:
-            self.test_dataset = LanceDataset(
-                self.lance_dirs[2],
-                batch_size=self.batch_size,
-                collate_fn=self.collate_fn,
+            test_sampler = ShardedBatchSampler(
                 rank=self.trainer.global_rank,
                 world_size=self.trainer.world_size,
+                randomize=False,
+                seed=self.seed,
+            )
+            self.val_dataset = LanceDataset(
+                self.lance_dirs[2],
+                batch_size=self.batch_size,
+                to_tensor_fn=self.to_tensor_fn,
+                sampler=test_sampler,
+                with_row_id=None,
             )
 
     def train_dataloader(self):
