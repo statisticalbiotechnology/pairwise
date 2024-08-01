ba662a946c924633818b9ff8e90bb5264defa52f
Alfred-N <alfredn@kth.se>
2024-08-01 19:10:26 +0200

    script for writing dino embeddings to ipc files

---
+++ src/ipc_dino_embeddings.py
@@ -0,0 +1,184 @@
+import torch
+import pytorch_lightning as pl
+import numpy as np
+import random
+from data.lance_data_module import LanceDataModule
+from ipc_to_embeddings import process_ipc_in_batches
+from parse_args import parse_args_and_config
+import os
+
+from wrappers.pretrain_wrappers import (
+    DinoTrainingPLWrapper,
+    MaskedAutoencoderWrapper,
+    MaskedTrainingPLWrapper,
+    TrinaryMZPLWrapper,
+)
+
+
+import utils
+
+import models.encoder as encoders
+import models.dc_encoder as dc_encoders
+
+ENCODER_DICT = {
+    **encoders.__dict__,
+    **dc_encoders.__dict__,
+}
+
+
+PRETRAIN_TASK_DICT = {
+    "masked": MaskedTrainingPLWrapper,
+    "masked_ae": MaskedAutoencoderWrapper,
+    "trinary_mz": TrinaryMZPLWrapper,
+    "dino": DinoTrainingPLWrapper,
+}
+
+
+class DINOEmbeddings(torch.nn.Module):
+    def __init__(self, pl_encoder) -> None:
+        super(DINOEmbeddings, self).__init__()
+        self.pl_wrapper = pl_encoder
+        self.encoder = pl_encoder.get_encoder()
+        self.cross_attn_token = pl_encoder.student.cross_attend_token
+        self.multihead_attn = pl_encoder.student.multihead_attn
+
+    def _pool_crossattend(self, embeds, padding_masks):
+        batch_size = embeds.shape[0]
+        cross_attend_tokens = self.cross_attn_token.expand(batch_size, -1, -1)
+        attn_output, _ = self.multihead_attn(
+            query=cross_attend_tokens,
+            key=embeds,
+            value=embeds,
+            key_padding_mask=padding_masks,
+        )
+        return attn_output.squeeze(1)
+
+    @torch.no_grad()
+    def forward(self, batch):
+        """
+        Args:
+            batch (dict):
+                        "mz_array": torch.Tensor of shape (batch_size, max_seq_len, embed_dim)
+                        "intensity_array": torch.Tensor of shape (batch_size, max_seq_len, embed_dim)
+                        "peak_lengths": torch.Tensor of shape (batch_size, 1)
+        """
+        mz_arr = batch["mz_array"]
+        int_arr = batch["intensity_array"]
+        mzab = torch.stack([mz_arr, int_arr], dim=-1)
+        lengths = batch["peak_lengths"]
+        padding_masks = self.pl_wrapper._get_padding_mask(mzab, lengths)
+
+        #
+        _out = self.encoder(mzab, key_padding_mask=padding_masks)
+        embeds = _out["emb"]
+        # These masks accomadate the new shape of embeds due to additional c/e/m or cls tokens
+        out_masks = _out["mask"]
+        embed = self._pool_crossattend(embeds, out_masks)  # (batch_size, embed_dim)
+        return embed
+
+
+def load_encoder(global_args, pretrain_config=None, ds_config=None):
+
+    config = {
+        **vars(global_args),
+        "downstream_config": ds_config,
+        "pretrain_config": pretrain_config,
+    }
+
+    # Define encoder model
+    encoder = ENCODER_DICT[global_args.encoder_model](
+        use_charge=global_args.use_charge,
+        use_mass=global_args.use_mass,
+        use_energy=global_args.use_energy,
+        dropout=config["pretrain_config"][global_args.pretraining_task].get(
+            "dropout", 0
+        ),
+        cls_token=global_args.cls_token,
+    )
+
+    if global_args.pretraining_task not in PRETRAIN_TASK_DICT:
+        raise NotImplementedError(
+            f"{global_args.pretraining_task} pretraining task not implemented"
+        )
+
+    print(f"Loaded encoder checkpoint: {global_args.encoder_weights}")
+    pl_encoder = PRETRAIN_TASK_DICT[global_args.pretraining_task].load_from_checkpoint(
+        global_args.encoder_weights,
+        global_args=global_args,
+        encoder=encoder,
+        task_dict=config["pretrain_config"][global_args.pretraining_task],
+    )
+    print(f"Loaded.")
+    embedder = DINOEmbeddings(pl_encoder)
+    embedder.eval()
+    return embedder
+
+
+def main(
+    embedder,
+    data_root_dir,
+    output_root_dir,
+    column_names,
+    batch_size=32,
+    max_peaks=1000,
+):
+    # List of IPC files to process
+    data_paths = [
+        "apis_mellifera.ipc",
+        "candidatus_endoloripes.ipc",
+        "h_sapiens.ipc",
+        "mus_musculus.ipc",
+    ]
+
+    # Process each file and write embeddings to new IPC files
+    for filename in data_paths:
+        input_path = os.path.join(data_root_dir, filename)
+        process_ipc_in_batches(
+            input_path=input_path,
+            output_dir=output_root_dir,
+            model=embedder,
+            column_names=column_names,
+            batch_size=batch_size,
+            max_peaks=max_peaks,
+        )
+        print(
+            f"Processed {filename} and saved to {os.path.join(output_root_dir, filename)}"
+        )
+
+
+if __name__ == "__main__":
+    # Parse args
+    global_args, pretrain_config, ds_config = parse_args_and_config()
+
+    # Set any specific settings like matmul precision
+    if global_args.matmul_precision:
+        torch.set_float32_matmul_precision(global_args.matmul_precision)
+
+    # Set random seeds for reproducibility
+    torch.manual_seed(global_args.seed)
+    np.random.seed(global_args.seed)
+    random.seed(global_args.seed)
+    pl.seed_everything(global_args.seed)
+
+    # Load the pre-trained encoder model wrapped for DINO embeddings
+    embedder = load_encoder(global_args, pretrain_config, ds_config)
+
+    # Define directories and column names
+    data_root_dir = "/Users/alfred/Datasets/9_species_IPC"
+    output_root_dir = os.path.join(data_root_dir, "embedded")
+    column_names = {
+        "mz": "mz_array",
+        "intensity": "intensity_array",
+        "charge": "precursor_charge",
+        "mass": "precursor_mz",
+    }
+
+    # Run the main function to process IPC files and generate embeddings
+    main(
+        embedder=embedder,
+        data_root_dir=data_root_dir,
+        output_root_dir=output_root_dir,
+        column_names=column_names,
+        batch_size=32,
+        max_peaks=1000,
+    )
